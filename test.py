dbe98f814413c92f12f6f9de5a11a08000fb8e31
jakubGryzio <82440709+jakubGryzio@users.noreply.github.com>
2021-05-26 09:03:09 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-test = np.array([0, 1, np.nan, np.nan])
-test2 = np.array([0, 1, 2, 3])
-print(test, test2)
-ind = np.where(np.isnan(test))
-test[ind] = test2[ind]
-print(test, test2)
